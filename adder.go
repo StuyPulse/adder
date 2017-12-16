d9d925e40e3b430b1b95be52dc0596114d736c8e
Justin Kim <j@justinkim.com>
2017-12-16 10:52:50 -0500

    Prep for Stuy Splash 2017

--- adder.go
+++ adder.go
@@ -4,10 +4,6 @@ func Add(n1, n2 int) int {
 	return n1 + n2
 }
 
-func Multiply(n1, n2 int) int {
-	return n1 * n2
-}
-
-func Subtract(n1, n2 int) int {
-	return n1 - n2
+func Divide(dividend, divisor int) int {
+	return dividend / divisor
 }
---
+++ cmd/math/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Team694/adder"
+)
+
+func main() {
+	sum := adder.Add(1, 1)
+	fmt.Println("1 + 1 =", sum)
+}
